86790205c54797f66602bd483385af9106df0354
Simon Unterluggauer <s.unterluggauer@navax.com>
2023-01-21 09:32:16 +0100

    added getGroupLed
    #2

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
@@ -1,7 +1,11 @@
 package at.edu.c02.ledcontroller;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public interface LedController {
     void demo() throws IOException;
+
+    public ArrayList<String> getGroupLed = null;
 }
